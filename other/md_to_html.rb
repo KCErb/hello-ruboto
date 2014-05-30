a74052f232fed6ed5b301c472dcfdce07d561ecf
KC Erb <iamkcerb@gmail.com>
2014-05-30 13:52:31 -0600

    updates

--- other/md_to_html.rb
+++ other/md_to_html.rb
@@ -2,6 +2,4 @@ require 'github/markup'
 
 file = 'lesson1.md'
 res = GitHub::Markup.render(file,File.read(file))
-res.gsub!("<pre lang=\"ruby\"><code>","[sourcecode language=\"ruby\"]")
-res.gsub!("</code></pre>","[/sourcecode]")
 File.open('lesson1.html', 'w') { |file| file.write(res) }
